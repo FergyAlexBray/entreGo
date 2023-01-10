1ce224e16bc4fc11b36c9ac8697af167508533ac
Segfault <charles.verchere@yahoo.fr>
2023-01-10 19:42:55 +0100

    feat: example of main

--- main.go
+++ main.go
@@ -1,11 +1,36 @@
 package main
 
 import (
+	"fmt"
+
 	entrego "github.com/FergyAlexBray/entreGo/src"
 )
 
 func main() {
 	core := entrego.Core{}
+
+	grid := [][]int{
+		{0, 1, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 1, 0},
+		{0, 1, 0, 1, 0, 0, 0},
+		{0, 0, 0, 1, 0, 0, 0},
+		{0, 0, 0, 1, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+	}
+
+	// define the start and end points
+	start := entrego.Point{0, 6}
+	end := entrego.Point{6, 6}
+
+	// find the shortest path
+	path := entrego.FindShortestPath(grid, start, end)
+
+	fmt.Println("Shortest path:")
+	//fmt.Printf("(%d)\n", path[1])
+	for _, p := range path {
+		fmt.Printf("(%d, %d)\n", p.X, p.Y)
+	}
 
 	entrego.Parser(&core)
 
